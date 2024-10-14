35c7e766378f327b0adb7898d448842dd5b2a340
atharvabharambe <atharvabharambebsl@gmail.com>
2024-10-14 07:53:00 +0530

    Test case 2 provided

--- src/testCase/test.java
+++ src/testCase/test.java
@@ -1,5 +1,6 @@
 package testCase;
 
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 
@@ -12,6 +13,19 @@ public class test {
 		driver.get("https://anon-buyonline.netlify.app/");
 		driver.manage().window().maximize();
 		System.out.println("Test case 1 passed");
+		
+//		Test case 2: Passing email to the pop up
+		if(true) {
+			driver.findElement(By.name("Subscribe-button")).click();
+			driver.findElement(By.name("email")).sendKeys("teamNSA");
+			driver.findElement(By.name("Subscribe-button")).click();
+//			driver.findElement(By.name("email")).clear();
+			driver.findElement(By.name("email")).sendKeys("@gmail.com");
+			System.out.println("Test case 2 passed");
+		}else {
+			System.out.println("Test case 2 failed");
+		}
+		
 		
 	}
 
