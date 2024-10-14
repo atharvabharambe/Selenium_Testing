eeba2dc3844f9a50c197925f7eafc8734c02c9b9
atharvabharambe <atharvabharambebsl@gmail.com>
2024-10-14 06:45:39 +0530

    initial commit

---
+++ src/testCase/test.java
@@ -0,0 +1,16 @@
+package testCase;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class test {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		WebDriver driver = new ChromeDriver();
+		driver.get("https://atharvabharambe.netlify.app/");
+		driver.manage().window().maximize();
+		
+	}
+
+}
