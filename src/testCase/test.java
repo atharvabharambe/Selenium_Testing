6a868203e645f6a1730ad9d34063fbf3e432276f
atharvabharambe <atharvabharambebsl@gmail.com>
2024-10-14 19:19:49 +0530

    Web Url Updated

--- src/testCase/test.java
+++ src/testCase/test.java
@@ -10,20 +10,27 @@ public class test {
 		// TODO Auto-generated method stub
 //		Test Case 1: Opening our website
 		WebDriver driver = new ChromeDriver();
-		driver.get("https://anon-buyonline.netlify.app/");
-		driver.manage().window().maximize();
+		driver.get("https://anon-shoponline.netlify.app/");
 		System.out.println("Test case 1 passed");
 		
-//		Test case 2: Passing email to the pop up
+//		Test case 2 : Maximize the webpage
+		driver.manage().window().maximize();
+		System.out.println("Test case 2 passed");
+		
+		
+		
+//		Test case 3: Passing email to the pop up
 		if(true) {
 			driver.findElement(By.name("Subscribe-button")).click();
 			driver.findElement(By.name("email")).sendKeys("teamNSA");
 			driver.findElement(By.name("Subscribe-button")).click();
 //			driver.findElement(By.name("email")).clear();
 			driver.findElement(By.name("email")).sendKeys("@gmail.com");
-			System.out.println("Test case 2 passed");
+			driver.findElement(By.name("Subscribe-button")).click();
+			System.err.println("Test case 3 passed");
+			
 		}else {
-			System.out.println("Test case 2 failed");
+			System.out.println("Test case 3 failed");
 		}
 		
 		
